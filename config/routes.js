612e703720e344eb19615e68f54a579e4b816af0
Elvis Ibarra <ielvisd@gmail.com>
2018-12-03 15:15:06 -0800

    MVP finished, Failed stretch React app added

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs'); // added package and required it here
+const db = require('../database/dbConfig.js');
 
-const { authenticate } = require('./middlewares');
+const { authenticate, generateToken } = require('./middlewares');
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -10,17 +12,45 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  // grab username and password from body
+  const creds = req.body;
+
+  // generate the hash from the user's password
+  const hash = bcrypt.hashSync(creds.password, 4); // rounds is 2^X
+
+  // override the user.password with the hash
+  creds.password = hash;
+
+  // save the user to the database
+  db('users')
+    .insert(creds)
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => res.json(err));
 }
 
 function login(req, res) {
   // implement user login
+  const creds = req.body;
+
+  db('users')
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ message: 'Logged in', token });
+      } else {
+        res.status(401).json({ message: 'You shall not pass!' });
+      }
+    })
+    .catch(err => res.status(500).json(err));
 }
 
 function getJokes(req, res) {
   axios
-    .get(
-      'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten'
-    )
+    .get('https://safe-falls-22549.herokuapp.com/random_ten')
     .then(response => {
       res.status(200).json(response.data);
     })
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const { server } = require('./server.js');
 
+server.get('/', (req, res) => {
+  console.log(routes);
+  res.status(200).json({ api: 'running' });
+});
+
 const port = process.env.PORT || 3300;
 server.listen(port, () => {
   console.log(`\n=== Server listening on port ${port}\n`);
